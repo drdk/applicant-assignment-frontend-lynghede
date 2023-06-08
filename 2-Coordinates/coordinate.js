58379cb6e88448c9287f1ae1a1e93c52da9174f1
Morten Lynghede <morten.lynghede@hotmail.com>
2023-06-08 12:40:29 +0200

    assignments 1, 2

--- 2-Coordinates/coordinate.js
+++ 2-Coordinates/coordinate.js
@@ -1,6 +1,57 @@
+class Coordinate {
+  constructor(param1, param2) {
+    if (param1 !== undefined && param2 !== undefined) {
+      this.x = param1;
+      this.y = param2;
+    } else if (param1 !== undefined) {
+      let sums = this.getPairSum(param1);
 
+      this.x = sums.x;
+      this.y = sums.y;
+    } else {
+      this.x = 0;
+      this.y = 0;
+    }
+  }
 
+  getPairSum(string) {
+    // Use regular expression to extract coordinate pairs
+    let pairs = string.match(/{\d+,\d+}/g);
 
-console.log ( new Coordinate( new Coordinate(30, 90) + new Coordinate(70, 150) ).toString() === '{100,240}' ); // Must return true.
+    let xSum = 0,
+      ySum = 0;
 
-console.log ( new Coordinate( new Coordinate(250, 10) + new Coordinate(60, 430) + new Coordinate(80, 0) ).toString() === '{390,440}' ); // Must return true.
+    pairs.forEach((pair) => {
+      // Remove braces and split pair into individual numbers
+      let [x, y] = pair.replace(/[{}]/g, "").split(",").map(Number);
+      xSum += x;
+      ySum += y;
+    });
+
+    return { x: xSum, y: ySum };
+  }
+
+  toString() {
+    return `{${this.x},${this.y}}`;
+  }
+}
+
+console.log(
+  new Coordinate(
+    new Coordinate(30, 90) + new Coordinate(70, 150)
+  ).toString() === "{100,240}"
+); // Must return true.
+
+console.log(
+  new Coordinate(
+    new Coordinate(250, 10) + new Coordinate(60, 430) + new Coordinate(80, 0)
+  ).toString() === "{390,440}"
+); // Must return true.
+
+console.log(
+  new Coordinate(
+    new Coordinate(50000, 50000) +
+      new Coordinate(50000, 50000) +
+      new Coordinate(50000, 50000)
+  ).toString() === "{150000,150000}"
+); // Must return true.
--- 3-ShowOff/README.md
+++ 3-ShowOff/README.md
@@ -1,7 +1,7 @@
 The TV department wants a prototype of how shows that are expiring soon can be presented.
 
 The API endpoint documentation can be found here:
-	http://www.dr.dk/mu-online/Help/1.3/Api/GET-api-apiVersion-list-view-lastchance_limit_offset_channel
+http://www.dr.dk/mu-online/Help/1.3/Api/GET-api-apiVersion-list-view-lastchance_limit_offset_channel
 
 **Assignment:** Create a simple single page application reading the API endpoint above using front-end technologies and/or Node.js.
 
@@ -9,6 +9,6 @@ There are many ways to solve this assignment, feel free to use templating engine
 
 Write well-structured semantic HTML5 and use CSS3 where it makes sense. The solution should be cross-browser compatible (IE11+) and mobile-friendly.
 
-*You're allowed to keep a static copy of the data feed in your solution to avoid cross-domain issues.*
+_You're allowed to keep a static copy of the data feed in your solution to avoid cross-domain issues._
 
-*Assignment Files:* None, add your own.
+_Assignment Files:_ None, add your own.
