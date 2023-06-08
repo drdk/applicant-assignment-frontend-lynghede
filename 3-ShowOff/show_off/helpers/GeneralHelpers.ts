473e6e3573fe2c7da104478c9513639bf7529a55
Morten Lynghede <morten.lynghede@hotmail.com>
2023-06-08 17:59:25 +0200

    wip design

--- 3-ShowOff/show_off/helpers/GeneralHelpers.ts
+++ 3-ShowOff/show_off/helpers/GeneralHelpers.ts
@@ -16,3 +16,22 @@ export function convertDateTime(date: string) {
 export function displayRating(rating: ParentalRating) {
   return rating.rating.includes("EXCEPT") ? "" : rating.description;
 }
+
+/**
+ * Converts runtime miliseconds to hours and minutes
+ * @param runtimeInMiliseconds
+ * @return string with X hours and X minutes
+ */
+export function convertRuntime(runtimeInMiliseconds: number) {
+  const numberToDate = new Date(runtimeInMiliseconds).toISOString();
+  const hours = numberToDate.substring(12, 13);
+  let min = numberToDate.substring(14, 16);
+  if (min.substring(0, 1) === "0") {
+    min = min.substring(1);
+  }
+
+  if (runtimeInMiliseconds < 3600000) {
+    return `${min} M`;
+  }
+  return `${hours} T ${min} M`;
+}
--- 3-ShowOff/show_off/src/app/page.tsx
+++ 3-ShowOff/show_off/src/app/page.tsx
@@ -1,28 +1,47 @@
 import Image from "next/image";
 import { getProgramData } from "../../services/Program";
-import { displayRating } from "../../helpers/GeneralHelpers";
+import { displayRating, convertRuntime } from "../../helpers/GeneralHelpers";
 
 export default async function Home() {
   const programData: Program[] = await getProgramData();
 
   return (
     <main>
-      <div>
+      <div className="space-y-4 p-4">
         {programData.map((program, index) => (
-          <div className="flex flex-col space-y-4 border-2 p-4" key={index}>
-            <h2 className="text-4xl font-bold">{program.title}</h2>
-            <h3 className="text-2xl">{program.description}</h3>
-            <Image
-              src={program.imageUri}
-              alt={program.title}
-              width={500}
-              height={500}
-            />
-            <div className="flex items-center text-2xl 2xl:text-3xl pt-2 tracking-wide space-x-2">
-              <p>{program.genre}</p>
-              <p>{program.startDate}</p>
-              <p>Udløber: {program.endDate}</p>
-              <p>{displayRating(program.parentalRating)}</p>
+          <div
+            className="relative flex flex-col space-y-4 border-2 rounded-sm"
+            key={index}>
+            <div
+              style={{
+                backgroundImage: `url(${program.imageUri})`,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "50% 50% !important",
+                overflow: "hidden",
+                height: "700px",
+              }}>
+              <div
+                className="h-full w-full absolute top-0 left-0"
+                style={{
+                  backgroundImage:
+                    "linear-gradient(0deg, #ff001e 5%, transparent)",
+                }}></div>
+            </div>
+            <div className="absolute bottom-12 flex flex-col z-10 p-8">
+              <h2 className="text-4xl font-bold">{program.title}</h2>
+              {/* <h3 className="text-2xl line-clamp-3">{program.description}</h3> */}
+              <div className="flex flex-col items-center text-2xl 2xl:text-3xl pt-2 tracking-wide space-x-2">
+                <div className="flex space-x-4 text-sm ">
+                  <p className="self-center">{program.genre.toUpperCase()}</p>
+                  <p className="border-[1px] p-1 text-sm">
+                    {displayRating(program.parentalRating).toUpperCase()}
+                  </p>
+                </div>
+                <p>Premiere: {program.startDate}</p>
+                <p>Udløber: {program.endDate}</p>
+                <p>{convertRuntime(program.runtime)}</p>
+              </div>
             </div>
           </div>
         ))}
